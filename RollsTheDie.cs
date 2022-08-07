b0b810eb9fe41226149071dbd7959daceec35539
jyoti <jyotidhage10@gmail.com>
2022-08-07 23:05:15 +0530

    [Aaradhya]Add:Player Rolls the Die

---
+++ RollsTheDie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public static class RollsTheDie
+    {
+       public static void RollAndDie()
+        {
+           
+            int position = 0;
+            Console.WriteLine("Start position of player: " + position);
+            Console.ReadLine();
+            Random random = new Random();
+            int dieValue = random.Next(1, 7);
+            Console.WriteLine(" Player die roll position = " + dieValue);
+            Console.ReadLine();
+        } 
+    }
+}
