4ed1d23882a86b02c6d5cfec0e6a32f5a97fd3a7
jyoti <jyotidhage10@gmail.com>
2022-08-07 23:01:41 +0530

    [Aaradhya]Add:Single Player at start position

---
+++ StartPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public static class StartPosition
+    {
+        public static void StartPosition()
+        {
+            int position = 0;
+            Console.WriteLine("Start position of player: " + position);
+            Console.ReadLine();
+        }
+    }      
+}
