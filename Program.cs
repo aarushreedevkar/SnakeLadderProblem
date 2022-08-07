c2e27ec021083ea1b3d68b3085e244a14023a2f1
jyoti <jyotidhage10@gmail.com>
2022-08-07 22:46:05 +0530

    Welcome to Snake Ladder Problem

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace SnakeLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("WECOME TO SNAKE AND LADDER PROBLEMS ");
+            //int position = 0;
+            //Console.WriteLine("Start position of player: " + position);
+            //Console.ReadLine();
+            StartPosition.StartPosition();
+            RollsTheDie.RollAndDie();
+            ChecksForOption.NoplayOption();            //methods calling
+            ExactWinningPosition.PlayerExactPosition();
+            PositionEveryDie.PositionOfPlayer();
+            RepeatTillWinning.TillWinningPosition();
+            TwoPlayerGame.TwoPlayer();
+            Game.Play();
+
+    {
+
+                {
+
+                {
+
+
+
+
+         
+        }
+    
+
+        
+    
+
